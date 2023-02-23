0625649ea87d913399e3980d8ca4f6bbc6525148
KornelSzyszka <kszyszka13@gmail.com>
2023-02-24 00:41:15 +0100

    Added Hevea Trees

--- src/main/java/net/kornel/pancraft/block/ModBlocks.java
+++ src/main/java/net/kornel/pancraft/block/ModBlocks.java
@@ -1,15 +1,20 @@
 package net.kornel.pancraft.block;
 
 import net.kornel.pancraft.PanCraft;
+import net.kornel.pancraft.block.custom.ModFlammableRotatedPillarBlock;
 import net.kornel.pancraft.item.ModCreativeModeTab;
 import net.kornel.pancraft.item.ModItems;
+import net.kornel.pancraft.world.feature.tree.HeveaTreeGrower;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -37,6 +42,60 @@ public class ModBlocks {
     public static final RegistryObject<Block> DEEPSLATE_NITER_ORE = registerBlock("deepslate_niter_ore",
             ()->new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)), ModCreativeModeTab.PANCRAFT_TAB);
+
+    public static final RegistryObject<Block> HEVEA_LOG = registerBlock("hevea_log",
+            ()->new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.PANCRAFT_TAB);
+    public static final RegistryObject<Block> HEVEA_WOOD = registerBlock("hevea_wood",
+            ()->new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.PANCRAFT_TAB);
+    public static final RegistryObject<Block> STRIPPED_HEVEA_LOG = registerBlock("stripped_hevea_log",
+            ()->new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.PANCRAFT_TAB);
+    public static final RegistryObject<Block> STRIPPED_HEVEA_WOOD = registerBlock("stripped_hevea_wood",
+            ()->new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_DARK_OAK_WOOD)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.PANCRAFT_TAB);
+
+    public static final RegistryObject<Block> HEVEA_PLANKS = registerBlock("hevea_planks",
+            ()->new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)
+                    .requiresCorrectToolForDrops()) {
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 5;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 20;
+                }
+            }, ModCreativeModeTab.PANCRAFT_TAB);
+    public static final RegistryObject<Block> HEVEA_LEAVES = registerBlock("hevea_leaves",
+            ()->new LeavesBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES)
+                    .requiresCorrectToolForDrops()) {
+                @Override
+                public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return true;
+                }
+
+                @Override
+                public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 30;
+                }
+
+                @Override
+                public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+                    return 60;
+                }
+            }, ModCreativeModeTab.PANCRAFT_TAB);
+
+    public static final RegistryObject<Block> HEVEA_SAPLING = registerBlock("hevea_sapling",
+            ()->new SaplingBlock(new HeveaTreeGrower()
+                    ,BlockBehaviour.Properties.copy(Blocks.OAK_SAPLING)), ModCreativeModeTab.PANCRAFT_TAB);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/kornel/pancraft/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/kornel/pancraft/world/feature/ModConfiguredFeatures.java
@@ -6,9 +6,15 @@ import net.kornel.pancraft.PanCraft;
 import net.kornel.pancraft.block.ModBlocks;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -33,6 +39,17 @@ public class ModConfiguredFeatures {
 
     public static final RegistryObject<ConfiguredFeature<?,?>> NITER_ORE = CONFIGURED_FEATURES.register("niter_ore",
             ()-> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_NITER_ORES.get(), 12)));
+
+    public static final RegistryObject<ConfiguredFeature<?,?>> HEVEA =
+            CONFIGURED_FEATURES.register("hevea", ()->
+                    new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
+                            BlockStateProvider.simple(ModBlocks.HEVEA_LOG.get()),
+                            new StraightTrunkPlacer(5,6,3),
+                            BlockStateProvider.simple(ModBlocks.HEVEA_LEAVES.get()),
+                            new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0),4),
+                            new TwoLayersFeatureSize(1,0,2)).build()));
+
+
 
     public static void register(IEventBus eventBus){
         CONFIGURED_FEATURES.register(eventBus);
